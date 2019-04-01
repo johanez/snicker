7e74a521323078ce396ba5e02e86a7b7068ee472
SamBrishes <sam@pytes.net>
2019-04-01 09:45:01 +0200

    Version 0.1.0

--- includes/autoload.php
+++ includes/autoload.php
@@ -1,4 +1,4 @@
-<?php   
+<?php
 /*
  |  Snicker     The first native FlatFile Comment Plugin 4 Bludit
  |  @file       ./includes/autoload.php
@@ -12,6 +12,8 @@
 
     spl_autoload_register(function($class){
         if(strpos($class, "Gregwar") === 0 || strpos($class, "Identicon") === 0  || strpos($class, "PIT") === 0){
+			$path = dirname(__FILE__) . DIRECTORY_SEPARATOR;
+			$class = str_replace("\\", DIRECTORY_SEPARATOR, $class);
             require_once $class . ".php";
         }
         return false;
